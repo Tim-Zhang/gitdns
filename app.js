a8276d88bb2fe6ade0bffb058540a7d9795e9644
TimZhang <timzhang@dnspod.com>
2013-04-03 15:24:29 +0000

    change node port to 5000

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ require('./db');
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 5000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.favicon());
