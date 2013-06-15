c03f8a8fa5b0a4d74aa12337cae18de92b047635
TimZhang <timzhang@dnspod.com>
2013-06-15 04:02:57 +0000

    fix obvious bug

--- config.js
+++ config.js
@@ -40,18 +40,18 @@ github.uri.push = 'github.dnsgit.com';
 if (env == 'development') {
   dnspod.key = '10021',
   dnspod.secret = '1bd198ae75f8e57b296c59532d55a9b9';
+  dnspod.uri.redirect = 'localhost:3000/oauth_callback';
 }
 // production only
 if (env == 'production') {
   dnspod.key = '10027',
   dnspod.secret = '0f3ccb8ff13c3858c78fabd77463dd0f';
-  github.uri.redirect = 'http://dnsgit.com/github_callback';
+  dnspod.uri.redirect = 'http://dnsgit.com/oauth_callback';
 }
 
 dnspod.uri.root = 'https://www.dnspod.cn';
 dnspod.uri.authorize = 'https://www.dnspod.cn/OAuth/Authorize';
-dnspod.uri.redirect = 'localhost:3000/oauth_callback';
-dnspod.uri.login = dnspod.uri.authorize + "?client_id=" + dnspod.key + "&redirect_uri" + dnspod.uri.redirect + "&response_type=code";
+dnspod.uri.login = dnspod.uri.authorize + "?client_id=" + dnspod.key + "&redirect_uri=" + dnspod.uri.redirect + "&response_type=code";
 dnspod.uri.accesstoken = 'https://www.dnspod.cn/OAuth/Access.Token';
 dnspod.uri.user = 'https://dnsapi.cn/User.Detail';
 
